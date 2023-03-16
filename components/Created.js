65a586b5080520fc22c020ee6616d71f4067b771
Imron Mirzarasulov <imronimronov32@gmail.com>
2023-03-16 13:48:04 +0500

    redesign wallet

--- components/Created.js
+++ components/Created.js
@@ -1,22 +1,31 @@
-import React from "react";
-import { WrappedButton } from "./Button";
-import { ScreenText } from "./ScreenText";
-import { ScreenTitle } from "./ScreenTitle";
-import { CREATE_SCREEN_TEXT_1, CREATE_SCREEN_TEXT_2, CREATE_SCREEN_TITLE } from "./text/constants";
+import React from 'react'
+import { WrappedButton } from './Button'
+import { ScreenText } from './ScreenText'
+import { ScreenTitle } from './ScreenTitle'
+import {
+	CREATE_SCREEN_TEXT_1,
+	CREATE_SCREEN_TEXT_2,
+	CREATE_SCREEN_TITLE,
+} from './text/constants'
 
 function Created() {
-  return (
-    <div id="created" className="screen visibility-hidden">
-      <div className="middle">
-        <div className="middle-content">
-          <ScreenTitle text={CREATE_SCREEN_TITLE}/>
-          <ScreenText text={CREATE_SCREEN_TEXT_1} />
-          <ScreenText text={CREATE_SCREEN_TEXT_2} />
-          <WrappedButton id='createdContinueButton' text='Continue' outerClassName='' innerClassName='btn-blue screen-btn mt-18 mb-20' />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div id="created" className="screen visibility-hidden">
+			<div className="middle">
+				<div className="middle-content">
+					<ScreenTitle text={CREATE_SCREEN_TITLE} />
+					<ScreenText text={CREATE_SCREEN_TEXT_1} />
+					<ScreenText text={CREATE_SCREEN_TEXT_2} />
+					<WrappedButton
+						id="createdContinueButton"
+						text="Continue"
+						outerClassName=""
+						innerClassName="btn-blue screen-btn mt-18 mb-20"
+					/>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default Created;
+export default Created
